8fe7ba505d04940b9414cb98946ff5c0ad7c9f93
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-06-05 22:02:06 +0200

    Add `onChange` handler to `email`

--- src/components/RegistrationPage.jsx
+++ src/components/RegistrationPage.jsx
@@ -6,13 +6,23 @@ class RegistrationPage extends Component {
     email: ""
   };
 
+  onEmailChange = e => {
+    const value = e.target.value;
+    this.setState(() => ({ email: value }));
+  };
+
   render() {
     return (
       <div>
         <h3>Create An Account</h3>
         <form>
           <label htmlFor="email">Email: </label>
-          <input type="email" name="email" value={this.state.email} />
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.onEmailChange}
+          />
           <label htmlFor="password">Password: </label>
           <input type="password" name="password" value={this.state.password} />
         </form>
