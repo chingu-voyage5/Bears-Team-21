ad7a5591c59891654ac947e6114ec1d7a9557a59
gtalin <aruj.joshi@gmail.com>
2018-06-12 18:44:35 +0100

    handle change

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,19 +2,50 @@ import React from 'react';
 class Login extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {email:'', password:''}
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    handleEmailChange(e) {
+        this.setState({email:e.target.value});
+    }
+    
+    handlePasswordChange (e){
+        this.setState({password:e.target.value});
+    }
+
+    handleLogin=(e)=>{
+        e.preventDefault();
+        console.log("handleLogin");
+        console.log(this.state);
+        //check with email and password in database
+        //authenticated or not display message
+        //go back to home page (if login not successful)
+        //go to messages page if login successful
+    }
+
+    handleCancel=(e)=>{
+        e.preventDefault();
+        //close form and go back to Home page
     }
 
     render() {
         return (
             <div>
                 <h3>Login</h3>
-                <form>
+                <form onSubmit={this.handleLogin}>
                     <label>Email Address:</label><br/>
-                    <input type="email" name="email"/><br/>
+                    <input type="email" name="email" 
+                    onChange={this.handleEmailChange} 
+                    /><br/>
                     <label>Code:</label><br/>
-                    <input type="password" name="password"/><br/>
-                    <button>Login</button>
-                    <button>Cancel</button>
+                    <input type="password" name="password" 
+                    onChange={this.handlePasswordChange} 
+                    /><br/>
+                    <button onClick={this.handleLogin}>Login</button>
+                    <button onClick={this.handleCancel}>Cancel</button>
                 </form>
             </div>
         )
